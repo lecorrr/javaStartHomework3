215f0480bc0d57d0260aa2372343421eac20af1b
lecor <88902282+lecorrr@users.noreply.github.com>
2025-03-04 23:51:41 +0200

    Update Main.java

--- thirdHomeWork/ThirdHomeWork/src/thirdpack/Main.java
+++ thirdHomeWork/ThirdHomeWork/src/thirdpack/Main.java
@@ -1,7 +1,7 @@
 package thirdpack;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
         System.out.println("Cost of driving");
 
